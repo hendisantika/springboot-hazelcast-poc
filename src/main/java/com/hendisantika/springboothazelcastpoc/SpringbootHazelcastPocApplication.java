97cd65900f429399947bbf53c3765422b3359486
hendisantika <hendisantika@yahoo.co.id>
2019-02-25 07:54:25 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springboothazelcastpoc/SpringbootHazelcastPocApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboothazelcastpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHazelcastPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHazelcastPocApplication.class, args);
+    }
+
+}
